b00dc9e7c9ab95106033b87da7c6bb12ef2f8cae
Daniela Araujo <90978184+danielascaraujo@users.noreply.github.com>
2024-09-22 18:35:04 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+// Links do AOS (animações) https://michalsnik.github.io/aos/
+// https://github.com/michalsnik/aos
+
+AOS.init();
+
+const eventDate = new Date("Jul 12, 2025 20:00:00");
+const eventTimeStamp = eventDate.getTime();
+
+const countTheTime = setInterval(function() {
+    const now = new Date();
+    const currentTimeStamp = now.getTime();
+
+    const timeUntilTheEvent = eventTimeStamp - currentTimeStamp;
+
+    const msDays = 1000 * 60 * 60 * 24;
+    const msHours = 1000 * 60 * 60;
+    const msMinutes = 1000 * 60;
+
+    const daysUntilTheEvent = Math.floor(timeUntilTheEvent / msDays);
+    const hoursUntilTheEvent = Math.floor((timeUntilTheEvent % msDays) / msHours);
+    const minutesUntilTheEvent = Math.floor((timeUntilTheEvent % msHours) / msMinutes);
+    const secondsUntilTheEvent = Math.floor((timeUntilTheEvent % msMinutes) / 1000);
+
+    document.getElementById('countdown').innerHTML = `Que começa em ${daysUntilTheEvent}d ${hoursUntilTheEvent}h ${minutesUntilTheEvent}m ${secondsUntilTheEvent}s`;
+
+    if (timeUntilTheEvent < 0) {
+        clearInterval(countTheTime);
+        document.getElementById('countdown').innerHTML = 'Que já acabou :('
+    }
+}, 1000);
